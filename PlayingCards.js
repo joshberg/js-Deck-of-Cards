4f35eb1000972d134917033bb5d88d5176aa16fe
joshberg <joshberg77@yahoo.com>
2017-02-20 23:12:00 -0600

    Added a DiscardPile

--- PlayingCards.js
+++ PlayingCards.js
@@ -161,3 +161,20 @@ class Deck {
     }
 }
 
+class DiscardPile{
+    constructor(){
+        this.Pile = [];
+    }
+    Add(Card){
+        this.Pile.push(Card);
+    }
+    GetAllCards(){
+        return this.Pile;
+    }
+    TopOfPile(){
+        return this.Pile[this.Pile.length-1];
+    }
+    Clear(){
+        this.Pile = [];
+    }
+}
